de3115b44fe18b73d101fef0a0d4bd4958eae1ad
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-11 11:28:17 -0400

    wrote moveShooter

--- app.js
+++ app.js
@@ -24,5 +24,19 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[currentShooterIndex].classList.add('shooter')
 
     //move the shooter
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter')
+        switch(e.keyCode){
+            case 37:
+                if(currentShooterIndex % width !== 0) currentShooterIndex-=1
+                break
+            case 39:
+                if(currentShooterIndex % width < width -1) currentShooterIndex+=1
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter')
+    }
+    document.addEventListener('keydown', moveShooter)
+
     
 })
